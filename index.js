7dea99c873c66b80739c01c82195936c752895bd
dwisecar <dwisecar@gmail.com>
2021-01-12 10:19:55 -0800

    commit tree 69c1ea84fd50ee671c28a3bf9b5dd6b95323a12c

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+// Write your solution in this file! 
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'JimBob'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Jim-Bob"
+}
